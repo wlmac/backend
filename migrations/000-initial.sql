dcdff4656b55501a682a21244cb7af0ec9bd0838
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-07-21 18:59:02 -0400

    add access level and change accType default

--- migrations/000-initial.sql
+++ migrations/000-initial.sql
@@ -7,9 +7,9 @@ lastName TEXT NOT NULL,
 email TEXT NOT NULL UNIQUE,
 password TEXT NOT NULL,
 grade INTEGER NOT NULL DEFAULT 0 CHECK((grade BETWEEN 9 AND 12) OR grade = 0 OR grade = -1),
-userType TEXT NOT NULL CHECK(userType = "student" OR userType = "teacher" OR userType = "admin"),
+userType TEXT NOT NULL DEFAULT "student" CHECK(userType = "student" OR userType = "teacher" OR userType = "admin"),
 profilePicture TEXT DEFAULT "noimg",
-generalAccessLevel INTEGER NOT NULL DEFAULT 0 CHECK(generalAccessLevel BETWEEN 0 AND 4)
+generalAccessLevel INTEGER NOT NULL DEFAULT 0 CHECK(generalAccessLevel BETWEEN -1 AND 4)
 );
 
 CREATE TABLE refresh(
