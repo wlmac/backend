f59b16861be6a2a00b4158acd89a22f29c47a158
ApocalypseCalculator <apocalypsecalculator@gmail.com>
2021-07-21 23:13:22 -0400

    camelcase gon

--- migrations/000-initial.sql
+++ migrations/000-initial.sql
@@ -2,14 +2,14 @@ PRAGMA foreign_keys = ON;
 
 CREATE TABLE users(
 userid TEXT PRIMARY KEY,
-firstName TEXT NOT NULL,
-lastName TEXT NOT NULL,
+firstname TEXT NOT NULL,
+lastname TEXT NOT NULL,
 email TEXT NOT NULL UNIQUE,
 password TEXT NOT NULL,
-gradyear INTEGER NOT NULL DEFAULT 0 CHECK(gradyear > 2020 OR grade = -1),
-userType TEXT NOT NULL DEFAULT "student" CHECK(userType = "student" OR userType = "teacher" OR userType = "admin"),
-profilePicture TEXT DEFAULT "noimg",
-generalAccessLevel INTEGER NOT NULL DEFAULT 0 CHECK(generalAccessLevel BETWEEN -1 AND 4)
+gradyear INTEGER NOT NULL DEFAULT 0 CHECK(gradyear > 2020 OR gradyear = -1),
+usertype TEXT NOT NULL DEFAULT "student" CHECK(usertype = "student" OR usertype = "teacher" OR usertype = "admin"),
+profilepicture TEXT DEFAULT "noimg",
+generalaccesslevel INTEGER NOT NULL DEFAULT 0 CHECK(generalaccesslevel BETWEEN -1 AND 4)
 );
 
 CREATE TABLE refresh(
