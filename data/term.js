7ec3bc6a7be684ccebc09798eec6fe8a64175c3b
Paul Lee <contact@paullee.dev>
2021-07-27 15:38:53 -0400

    add dayinfo/get endpoint

---
+++ data/term.js
@@ -0,0 +1,37 @@
+module.exports = {
+    year: 2021,
+    term: 1,
+    days: 4,
+    startDate: [2021, 06, 28],
+    endDate: [2021, 09, 04],
+    events: [
+        {
+            name: 'Department Meeting',
+            startDate: [2021, 07, 15],
+            endDate: [2021, 07, 16],
+            isEarlyDismissal: true,
+            isHoliday: false
+        },
+        {
+            name: 'Summer Break',
+            startDate: [2021, 07, 31],
+            endDate: [2021, 08, 09],
+            isEarlyDismissal: false,
+            isHoliday: true
+        },
+        {
+            name: 'A Perfectly Normal Day',
+            startDate: [2021, 07, 06],
+            endDate: [2021, 07, 07],
+            isEarlyDismissal: false,
+            isHoliday: false
+        },
+        {
+            name: 'AmogUs Weeks',
+            startDate: [2021, 08, 16],
+            endDate: [2021, 08, 28],
+            isEarlyDismissal: true,
+            isHoliday: false
+        }
+    ]
+}
---
+++ endpoints/dayinfoget.js
@@ -0,0 +1,107 @@
+const helper = require('../lib/helper');
+const config = require('../config.json');
+const term = require('../data/term');
+
+function newDate(dateString) {
+    // Hacky but what can I do?
+    let result = new Date(`${dateString} UTC-4`);
+
+    return result;
+}
+
+function isEventWithinDateRange(rangeStart, rangeEnd, eventDate) {
+    let startDate = newDate(rangeStart);
+    let endDate = newDate(rangeEnd);
+
+    return startDate <= eventDate && eventDate < endDate;
+}
+
+function getEvents(date) {
+    return term.events.filter(dayEvent => isEventWithinDateRange(dayEvent.startDate, dayEvent.endDate, date));
+}
+
+function getDayStatus(date) {
+    const dayEvents = getEvents(date);
+    let result = {isHoliday: false, isEarlyDismissal: false, reason: null};
+
+    if (!isEventWithinDateRange(term.startDate, term.endDate, date)) {
+        result.isHoliday = true;
+        result.reason = "No ongoing term";
+
+        return result;
+    }
+
+    for (const dayEvent of dayEvents) {
+        if (dayEvent.isHoliday) {
+            result.isHoliday = true;
+            result.reason = dayEvent.name;
+        } else if (dayEvent.isEarlyDismissal) {
+            result.isEarlyDismissal = true;
+            if (!result.isHoliday || result.reason == null) result.reason = dayEvent.name;
+        } else if (result.reason == null) {
+            result.reason = dayEvent.name;
+        }
+    }
+
+    if (date.getDay() % 6 == 0) {
+        if (!result.isHoliday || result.reason == null) result.reason = "Weekend";
+        result.isHoliday = true;
+    }
+
+    return result;
+}
+
+function getDayNumber(date) {
+    if (!isEventWithinDateRange(term.startDate, term.endDate, date)) return null;
+
+    let dayNum = 0;
+    let currentDay = newDate(term.startDate);
+
+    if (getDayStatus(date).isHoliday) return null;
+
+    while (currentDay < date) {
+        if (!getDayStatus(currentDay).isHoliday) dayNum += 1;
+        currentDay.setDate(currentDay.getDate() + 1);
+    }
+
+    return dayNum % term.days + 1;
+}
+
+module.exports.name = "dayinfo/get";
+module.exports.verify = function (req, res) {
+    return true;
+}
+
+module.exports.execute = function (req, res) {
+    if (!("date" in req.body)) {
+        res.status(400);
+        res.send({status: 400, error: "Missing required field"});
+
+        return;
+    }
+    
+    try {
+        let result = {day: null, status: null, description: null};
+        let date = newDate(req.body.date);
+
+        result.day = getDayNumber(date);
+        result.status = getDayStatus(date);
+
+        if (result.day == null) {
+            result.description = `No School - ${result.status.reason}`;
+        } else if (result.status.isEarlyDismissal) {
+            result.description = `Day ${result.day} - Early Dismissal - ${result.status.reason}`;
+        } else if (result.status.reason != null) {
+            result.description = `Day ${result.day} - ${result.status.reason}`;
+        } else {
+            result.description = `Day ${result.day}`;
+        }
+
+        res.status(200);
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+        res.status(500);
+        res.send({status: 500, error: "Internal server error"});
+    }
+}
